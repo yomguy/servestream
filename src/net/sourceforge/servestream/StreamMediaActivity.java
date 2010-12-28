5b04969ca0a93a11fa1f503fe476b8bd5421e533
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2010-12-28 09:42:02 +0000

    Added code to allow playing music in background
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@109 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/StreamMediaActivity.java
+++ src/net/sourceforge/servestream/StreamMediaActivity.java
@@ -17,58 +17,61 @@
 
 package net.sourceforge.servestream;
 
-import java.util.ArrayList;
-
-import net.sourceforge.servestream.custom.CustomVideoView;
+import net.sourceforge.servestream.R;
 import net.sourceforge.servestream.service.MusicService;
-import net.sourceforge.servestream.utils.PlaylistHandler;
-import net.sourceforge.servestream.utils.PreferenceConstants;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
-import android.media.MediaPlayer.OnErrorListener;
-import android.media.MediaPlayer.OnPreparedListener;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
+import android.os.Message;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View.OnClickListener;
-import android.widget.MediaController;
+import android.view.View.OnTouchListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class StreamMediaActivity extends Activity {
-	public final static String TAG = "ServeStream.StreamMediaActivity";
-	
+public class StreamMediaActivity extends Activity implements SurfaceHolder.Callback {
+    private static final String TAG = "ServeStream.StreamMediaActivity";
+    
+	private static final int MEDIA_CONTROLS_DISPLAY_TIME = 2000;
+    
 	private int displayWidth = 0;
 	private int displayHeight = 0;
+    
+	private String requestedStream = "";
+	private String currentlyPlayingStream = "";
 	
-    private ArrayList<String> mediaFiles = null;
-	private int mediaFilesIndex = 0;
-	
-	private int savedIndex = -1;
-	private int savedMediaPosition = -1;
-	private boolean resumePlaying = false; 
-	
-	private MediaController mediaController = null;
-	private CustomVideoView videoView = null;
-	
-	private SharedPreferences preferences = null;	
+    private MediaPlayer mediaPlayer;
+
+	private Animation controller_fade_in, controller_fade_out;
+	private RelativeLayout mediaControllerGroup;
+    
+    private SurfaceView preview;
+    private SurfaceHolder holder;
+
     private ProgressDialog dialog = null;
-	
+    
+	private Handler handler = new Handler();
+    
 	private MusicService boundService;
 
 	private ServiceConnection connection = new ServiceConnection() {
@@ -79,6 +82,8 @@ public class StreamMediaActivity extends Activity {
 	        // service that we know is running in our own process, we can
 	        // cast its IBinder to a concrete class and directly access it.
 	        boundService = ((MusicService.MusicBinder)service).getService();
+	        boundService.setNowPlayingHandler(nowPlayingHandler);
+	        //boundService.setProgressDialog(dialog);
 	    }
 
 	    public void onServiceDisconnected(ComponentName className) {
@@ -89,59 +94,13 @@ public class StreamMediaActivity extends Activity {
 	        boundService = null;
 	    }
 	};
-	
-	@Override
-	public void onPause() {
-		super.onPause();
-		
-		if (videoView != null) {
-		    if (videoView.isPlaying()) {
-			    savedIndex = mediaFilesIndex;
-			    savedMediaPosition = videoView.getCurrentPosition();
-			    videoView.stopPlayback();
-			    resumePlaying = true;
-		    } else {
-			    resumePlaying = false;
-		    }
+    
+	protected Handler nowPlayingHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+	        Toast.makeText(StreamMediaActivity.this, "Now Playing: " + msg.obj, Toast.LENGTH_LONG).show();
 		}
-		
-		/*if (videoView.isPlaying()) {
-		    videoView.stopPlayback();
-            try {
-			    m_boundService.startMusicInBackground(mediaFiles, mediaFilesIndex, 
-					    videoView.getCurrentPosition());
-		    } catch (Exception e) {
-			    e.printStackTrace();
-		    }
-		}*/
-	}	
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		
-		if (resumePlaying) {
-	    	mediaFilesIndex = savedIndex;
-	        videoView.setVideoURI(Uri.parse(mediaFiles.get(mediaFilesIndex)));
-	        videoView.start();
-	        videoView.seekTo(savedMediaPosition);
-	        resumePlaying = false;
-			dialog = ProgressDialog.show(StreamMediaActivity.this, "", 
-	                "Resuming playback...", true);
-		}
-		
-		/*if (m_boundService != null) {
-		    try {
-			    m_boundService.stopMusicInBackground();
-		    } catch (Exception e) {
-			    e.printStackTrace();
-		    }
-		
-		    videoView.setVideoURI(Uri.parse(mediaFiles.get(m_boundService.getCurrentMediaFileIndex())));
-		    videoView.seekTo(m_boundService.getCurrentMediaFileIndex());
-		    videoView.start();
-		}*/
-	}
+	};
 	
     @Override
     public void onCreate(Bundle icicle) {
@@ -151,55 +110,119 @@ public class StreamMediaActivity extends Activity {
 		this.setTitle(String.format("%s: %s",
 				getResources().getText(R.string.app_name),
 				getResources().getText(R.string.title_stream_play))); 
-        
+		
 		// get the phone's display width and height
 		getDisplayMeasurements();
 		
-        String stream = getIntent().getExtras().getString("net.sourceforge.servestream.TargetStream");
+		requestedStream = getIntent().getExtras().getString("net.sourceforge.servestream.TargetStream");
         
-		preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		//preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		// preload animation for media controller display
+		controller_fade_in = AnimationUtils.loadAnimation(this, R.anim.controller_fade_in);
+		controller_fade_out = AnimationUtils.loadAnimation(this, R.anim.controller_fade_out);
         
-        if (isPlaylist(stream)) {
-            PlaylistHandler playlistHandler = new PlaylistHandler(stream);
-            playlistHandler.buildPlaylist();
-            mediaFiles = playlistHandler.getPlayListFiles();
-        } else {
-        	mediaFiles = new ArrayList<String>();
-        	mediaFiles.add(stream);
-        }
+		mediaControllerGroup = (RelativeLayout) findViewById(R.id.media_controller_group);
+		mediaControllerGroup.setVisibility(View.GONE);
         
-        videoView = (CustomVideoView) findViewById(R.id.surface_view);
-        videoView.setDimensions(displayWidth, displayHeight);
-        videoView.setOnPreparedListener(new OnPreparedListener() {
-			public void onPrepared(MediaPlayer mp) {
-				dialog.dismiss();
-		        Toast.makeText(StreamMediaActivity.this, "Now Playing: " + mediaFiles.get(mediaFilesIndex), Toast.LENGTH_LONG).show();
+        preview = (SurfaceView) findViewById(R.id.surface_view);
+        preview.setOnTouchListener(new OnTouchListener() {
+
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				if (mediaControllerGroup.isShown()) {
+				    mediaControllerGroup.startAnimation(controller_fade_out);
+				    mediaControllerGroup.setVisibility(View.GONE);
+				} else {
+					mediaControllerGroup.startAnimation(controller_fade_in);
+					mediaControllerGroup.setVisibility(View.VISIBLE);
+					
+					handler.postDelayed(new Runnable() {
+						public void run() {
+							if (mediaControllerGroup.getVisibility() == View.GONE)
+								return;
+
+							mediaControllerGroup.startAnimation(controller_fade_out);
+							mediaControllerGroup.setVisibility(View.GONE);
+						}
+					}, MEDIA_CONTROLS_DISPLAY_TIME);
+				}
+				return false;
 			}
-        });
+		});
         
-        // attempt to get data from before device configuration change
-        Bundle returnData = (Bundle) getLastNonConfigurationInstance();
+        holder = preview.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setDisplay(holder);
+        holder.setFixedSize(displayWidth, displayHeight);
         
-        if (returnData == null) {
-            if (mediaFiles.size() == 0) {
-    			new AlertDialog.Builder(StreamMediaActivity.this)
-    			.setTitle(R.string.cannot_play_media_title)
-    			.setMessage("The selected playlist file has no valid song entries.")
-    			.setPositiveButton(R.string.cannot_play_media_pos, new DialogInterface.OnClickListener() {
-    				public void onClick(DialogInterface dialog, int which) {
-    					finish();
-    				}
-    				}).create().show();
-            } else {
-                mediaController = new MediaController(this, true);
-                mediaController.setPrevNextListeners(null, null);
-                videoView.setMediaController(mediaController);
-            
-                videoView.setOnErrorListener(m_onErrorListener);
-                videoView.setOnCompletionListener(m_onCompletionListener);
-                startSong(mediaFilesIndex);
-            }
-        }
+		Button previousButton = (Button) findViewById(R.id.previous_button);
+		previousButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				boundService.previousMediaFile();
+				
+			    mediaControllerGroup.startAnimation(controller_fade_out);
+				mediaControllerGroup.setVisibility(View.GONE);
+			}
+			
+		});
+
+		Button seekBackwardButton = (Button) findViewById(R.id.seek_backward_button);
+		seekBackwardButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				boundService.seekBackward();
+				
+			    mediaControllerGroup.startAnimation(controller_fade_out);
+				mediaControllerGroup.setVisibility(View.GONE);
+			}
+			
+		});
+		
+		final Button playPauseButton = (Button) findViewById(R.id.play_pause_button);
+		playPauseButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				if (boundService.isPlaying()) {
+					boundService.pauseMedia();
+					playPauseButton.setBackgroundResource(R.drawable.play_button);
+				} else {
+					boundService.resumeMedia();
+					playPauseButton.setBackgroundResource(R.drawable.pause_button);
+				}
+				
+			    mediaControllerGroup.startAnimation(controller_fade_out);
+				mediaControllerGroup.setVisibility(View.GONE);
+			}
+			
+		});
+		
+		Button seekForwardButton = (Button) findViewById(R.id.seek_forward_button);
+		seekForwardButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				boundService.seekForward();
+				
+			    mediaControllerGroup.startAnimation(controller_fade_out);
+				mediaControllerGroup.setVisibility(View.GONE);
+			}
+			
+		});
+		
+		Button nextButton = (Button) findViewById(R.id.next_button);
+		nextButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				boundService.nextMediaFile();
+				
+			    mediaControllerGroup.startAnimation(controller_fade_out);
+				mediaControllerGroup.setVisibility(View.GONE);
+			}
+			
+		});
     }
     
 	@Override
@@ -208,19 +231,26 @@ public class StreamMediaActivity extends Activity {
 		
 		// connect with manager service to find all bridges
 		// when connected it will insert all views
-		//bindService(new Intent(this, MusicService.class), connection, Context.BIND_AUTO_CREATE);
+		bindService(new Intent(this, MusicService.class), connection, Context.BIND_AUTO_CREATE);
 	}
 	
 	@Override
 	public void onStop() {
 		super.onStop();
 		
-        //unbindService(connection);
+        unbindService(connection);
 	}
     
 	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+
+		requestedStream = getIntent().getExtras().getString("net.sourceforge.servestream.TargetStream");
+	}
+	
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		//super.onCreateOptionsMenu(menu);
+		super.onCreateOptionsMenu(menu);
 
 		MenuItem settings = menu.add(R.string.list_menu_settings);
 		settings.setIcon(android.R.drawable.ic_menu_preferences);
@@ -232,13 +262,13 @@ public class StreamMediaActivity extends Activity {
 
 		return true;
 	}
-	
+    
     @Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		
-		if (videoView.isInTouchMode()) {
-			mediaController.hide();
+		if (mediaControllerGroup.isShown()) {
+			mediaControllerGroup.setVisibility(View.GONE);
 		}
 		
 		// get new window size on orientation change
@@ -247,100 +277,41 @@ public class StreamMediaActivity extends Activity {
 	    if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
 	        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	        videoView.setDimensions(displayWidth, displayHeight);
-	        videoView.getHolder().setFixedSize(displayWidth, displayHeight);
+	        holder.setFixedSize(displayWidth, displayHeight);
 	    } else {
 	        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-	        videoView.setDimensions(displayWidth, displayHeight);
-	        videoView.getHolder().setFixedSize(displayWidth, displayHeight);
+	        holder.setFixedSize(displayWidth, displayHeight);
 	    }
 	}
-    
-    private OnCompletionListener m_onCompletionListener = new OnCompletionListener() {
-		public void onCompletion(MediaPlayer mp) {
 
-			// if repeat preference is set to one, play the media file again
-			if (preferences.getString(PreferenceConstants.REPEAT, "Off").equals("One")) {
-				startSong(mediaFilesIndex);
-				return;
-			}
-			
-			mediaFilesIndex++;
-			
-			if (mediaFilesIndex == mediaFiles.size()) {
-				if (preferences.getString(PreferenceConstants.REPEAT, "Off").equals("All")) {
-					startSong(0);
-					return;
-				}
-				finish();
-			} else {
-                startSong(mediaFilesIndex);
-			}
-		}
-    };
-	
-    private OnErrorListener m_onErrorListener = new OnErrorListener() {
-
-		public boolean onError(MediaPlayer arg0, int arg1, int arg2) {
-			new AlertDialog.Builder(StreamMediaActivity.this)
-			.setTitle(R.string.cannot_play_media_title)
-			.setMessage(R.string.cannot_play_media_message)
-			.setPositiveButton(R.string.cannot_play_media_pos, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					finish();
-				}
-				}).create().show();
-			return true;
-		}
-    };
-    
-    private class previousOnClickListener implements OnClickListener {
-
-		public void onClick(View arg0) {
-            startSong(mediaFilesIndex - 1);
-		}
+    public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+        Log.d(TAG, "surfaceChanged called");
     }
-    
-    private class nextOnClickListener implements OnClickListener {
 
-		public void onClick(View arg0) {
-            startSong(mediaFilesIndex + 1);
-		}
+    public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+        Log.d(TAG, "surfaceDestroyed called");
     }
-    
-    /**
-     * Sets the state of the previous and next buttons on the
-     * media player
-     */
-    private void setPlayerButtonStates() {
-    	if (!(mediaFiles.size() == 1)) {
-	 	    if (mediaFilesIndex == 0) {
-                mediaController.setPrevNextListeners(new nextOnClickListener(), null);
-		    } else if (mediaFilesIndex == mediaFiles.size() - 1) {
-                mediaController.setPrevNextListeners(null, new previousOnClickListener());   	
-		    } else {
-                mediaController.setPrevNextListeners(new nextOnClickListener(), new previousOnClickListener());
-		    }
-    	}
-    }
-    
-    /**
-     * 
-     * 
-     * @param playlistFileIndex The index of the file to play
-     */
-    private void startSong(int playlistFileIndex) {
-    	
-    	mediaFilesIndex = playlistFileIndex;
-		setPlayerButtonStates();
-    	
-		videoView.stopPlayback();
-        videoView.setVideoURI(Uri.parse(mediaFiles.get(mediaFilesIndex)));
-        videoView.start();
-        
-		dialog = ProgressDialog.show(StreamMediaActivity.this, "", 
-                "Opening file...", true);
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated called");
+
+        if (!requestedStream.equals(currentlyPlayingStream)) {
+            try {
+            	
+            	if (boundService.getMediaPlayer() == null) {
+                    boundService.setMediaPlayer(mediaPlayer);
+            	} else {
+                    boundService.stopMedia();
+            	}
+            	
+                boundService.queueNewMedia(requestedStream);
+                boundService.startMediaPlayer();
+            } catch (Exception e) {
+                Log.e(TAG, "error: " + e.getMessage(), e);
+            }
+            currentlyPlayingStream = requestedStream;
+        }
     }
     
     /**
@@ -350,21 +321,5 @@ public class StreamMediaActivity extends Activity {
         Display display = getWindowManager().getDefaultDisplay();
         displayWidth = display.getWidth();
         displayHeight = display.getHeight();
-    }
-    
-    /**
-     * Checks if a file is a playlist file
-     * 
-     * @param mediaFileName The file to check
-     * @return boolean True if the file is a playlist, false otherwise
-     */
-    private boolean isPlaylist(String mediaFileName) {
-    	if (mediaFileName.length() > 4) {
-    	    if (mediaFileName.substring(mediaFileName.length() - 4, mediaFileName.length()).equalsIgnoreCase(".m3u")) {
-    	    	return true;
-    	    }
-    	}
-    	
-    	return false;
     }
 }
