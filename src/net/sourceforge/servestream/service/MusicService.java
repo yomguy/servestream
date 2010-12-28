7c45ba84b2a229d973bdf2a4dadb6d3cdf3769ed
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2010-12-28 09:41:49 +0000

    Added code to allow playing music in background
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@108 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/service/MusicService.java
+++ src/net/sourceforge/servestream/service/MusicService.java
@@ -19,21 +19,34 @@ package net.sourceforge.servestream.service;
 
 import java.util.ArrayList;
 
+import net.sourceforge.servestream.utils.PlaylistHandler;
+import net.sourceforge.servestream.utils.PreferenceConstants;
+
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class MusicService extends Service {
 	public final static String TAG = "ServeStream.MusicService";
+	
+    private ArrayList<String> mediaFiles = null;
+	private int mediaFilesIndex = 0;
+	
+    private MediaPlayer mediaPlayer = null;
     
-    private ArrayList<String> m_mediaFiles = null;
-	private int m_currentMediaIndex = 0;
-	private int m_mediaPosition = 0;
-    private MediaPlayer m_mediaPlayer = null;
-
+    private Handler nowPlayingHandler;
+    
+	private SharedPreferences preferences = null;
+    //private ProgressDialog dialog = null;
+    
     /**
      * Class for clients to access.  Because we know this service always
      * runs in the same process as its clients, we don't need to deal with
@@ -48,7 +61,7 @@ public class MusicService extends Service {
     @Override
     public void onCreate() {
 
-    	m_mediaPlayer = new MediaPlayer();
+		preferences = PreferenceManager.getDefaultSharedPreferences(this);
     	
         // Display a notification about us starting.  We put an icon in the status bar.
 		ConnectionNotifier.getInstance().showRunningNotification(this);
@@ -56,6 +69,7 @@ public class MusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+    	
         Log.i("MusicService", "Received start id " + startId + ": " + intent);
         // We want this service to continue running until it is explicitly
         // stopped, so return sticky.
@@ -63,65 +77,187 @@ public class MusicService extends Service {
     }
 
     @Override
-    public void onDestroy() {
+    public void onDestroy() {  	
     	
         // Cancel the persistent notification.
 		ConnectionNotifier.getInstance().hideRunningNotification(this);
-		Log.v("should have rmoved isonc","ok");
     }
 
+    // This is the object that receives interactions from clients.  See
+    // RemoteService for a more complete example.
+    private final IBinder binder = new MusicBinder();
+    
     @Override
     public IBinder onBind(Intent intent) {
-
+    	
 		// Make sure we stay running to maintain the bridges
 		startService(new Intent(this, MusicService.class));
     	
-        return m_binder;
+        return binder;
     }
     
     @Override
     public boolean onUnbind(Intent intent) {
 		
-		stopSelf();
+		if (!mediaPlayer.isPlaying()) {
+			releaseMediaPlayer();
+			stopSelf();
+		}
     	
 		return true;
     }
+    
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();	
+    }
+    
+    private void releaseMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+    
+    public MediaPlayer getMediaPlayer() {
+    	return this.mediaPlayer;
+    }
+    
+    public void setMediaPlayer(MediaPlayer mediaPlayer) {
+    	this.mediaPlayer = mediaPlayer;
+    	this.mediaPlayer.setOnCompletionListener(m_onCompletionListener);
+    }
+    
+    public void startMediaPlayer() {
+        startMedia(0);
+    }
+    
+    public void nextMediaFile() {
+    	
+    	if (mediaFilesIndex == (mediaFiles.size() - 1)) {
+    		startMedia(0);
+    	} else {
+            startMedia(mediaFilesIndex + 1);
+    	}
+    }
+    
+    public void pauseMedia() {
+    	mediaPlayer.pause();
+    }
+    
+    public void resumeMedia() {
+    	mediaPlayer.start();
+    }
+    
+    public void previousMediaFile() {
+    	if (mediaFilesIndex == 0) {
+    		startMedia((mediaFiles.size() - 1));
+    	} else {
+            startMedia((mediaFilesIndex - 1));
+    	}
+    }
+    
+    /*public void setProgressDialog(ProgressDialog dialog) {
+    	this.dialog = dialog;
+    }*/
+    
+    public void setNowPlayingHandler(Handler handler) {
+    	this.nowPlayingHandler = handler;
+    }
+    
+    public void seekBackward() {
+    	
+    	int seekPosition = mediaPlayer.getCurrentPosition() - 15000;
+    	
+    	if (seekPosition >= 0) {
+    		mediaPlayer.seekTo(seekPosition);
+    	} else {
+    		mediaPlayer.seekTo(0);    		
+    	}
+    }
 
-    // This is the object that receives interactions from clients.  See
-    // RemoteService for a more complete example.
-    private final IBinder m_binder = new MusicBinder();
+    public void seekForward() {
+    	
+    	int seekPosition = mediaPlayer.getCurrentPosition() + 15000;
+    	
+    	if (seekPosition < mediaPlayer.getDuration()) {
+    		mediaPlayer.seekTo(seekPosition);
+    	} else {
+    		mediaPlayer.seekTo(mediaPlayer.getDuration());    		
+    	}
+    }
+    
+    private void startMedia(int index) {
+    	mediaFilesIndex = index;
+    	
+    	try {
+	        mediaPlayer.reset();
+	        mediaPlayer.setDataSource(mediaFiles.get(mediaFilesIndex));
+	        mediaPlayer.prepare();
+		    mediaPlayer.start();
+		    nowPlayingHandler.sendMessage(Message.obtain(nowPlayingHandler, 1, mediaFiles.get(mediaFilesIndex)));
+		    Log.v(TAG, "Starting media file");
+		    //dialog.dismiss();
+    	} catch (Exception ex) {
+    		ex.printStackTrace();
+    	}
+    }
+    
+    public void stopMedia() {
+    	mediaPlayer.stop();
+    	mediaPlayer.reset();
+    }
+    
+    private OnCompletionListener m_onCompletionListener = new OnCompletionListener() {
+		public void onCompletion(MediaPlayer mp) {
 
+			// if repeat preference is set to one, play the media file again
+			if (preferences.getString(PreferenceConstants.REPEAT, "Off").equals("One")) {
+				startMedia(mediaFilesIndex);
+				return;
+			}
+			
+			mediaFilesIndex++;
+			
+			if (mediaFilesIndex == mediaFiles.size()) {
+				if (preferences.getString(PreferenceConstants.REPEAT, "Off").equals("All")) {
+					startMedia(0);
+					return;
+				}
+				releaseMediaPlayer();
+				stopSelf();
+			} else {
+                startMedia(mediaFilesIndex);
+			}
+		}
+    };
+    
+    public boolean queueNewMedia(String requestedStream) {
+    
+        if (isPlaylist(requestedStream)) {
+            PlaylistHandler playlistHandler = new PlaylistHandler(requestedStream);
+            playlistHandler.buildPlaylist();
+            mediaFiles = playlistHandler.getPlayListFiles();
+        } else {
+        	mediaFiles = new ArrayList<String>();
+        	mediaFiles.add(requestedStream);
+        }
+    	
+    	return true;
+    }
+    
     /**
-     * Show a notification while this service is running.
+     * Checks if a file is a playlist file
+     * 
+     * @param mediaFileName The file to check
+     * @return boolean True if the file is a playlist, false otherwise
      */
-    private void showNotification() {
-		ConnectionNotifier.getInstance().showRunningNotification(this);
-    }
-    
-    public void startMusicInBackground(ArrayList<String> mediaFiles, int currentMediaIndex , int currentPosition) throws Exception {    	
- 
-    	Log.v("current pos", String.valueOf(currentPosition));
+    private boolean isPlaylist(String mediaFileName) {
+    	if (mediaFileName.length() > 4) {
+    	    if (mediaFileName.substring(mediaFileName.length() - 4, mediaFileName.length()).equalsIgnoreCase(".m3u")) {
+    	    	return true;
+    	    }
+    	}
     	
-    	this.m_currentMediaIndex = currentMediaIndex;
-    	
-    	m_mediaPlayer.setDataSource(mediaFiles.get(currentMediaIndex));
-    	m_mediaPlayer.prepare();
-    	m_mediaPlayer.start();
-    	m_mediaPlayer.seekTo(currentPosition);
-    }
-    
-    public void stopMusicInBackground() throws Exception {    	
-    	
-    	this.m_mediaPosition = m_mediaPlayer.getCurrentPosition();
-
-    	m_mediaPlayer.stop();
-    }
-    
-    public int getCurrentPosition() {
-    	return this.m_mediaPosition;
-    }
-    
-    public int getCurrentMediaFileIndex() {
-    	return this.m_currentMediaIndex;
+    	return false;
     }
 }
