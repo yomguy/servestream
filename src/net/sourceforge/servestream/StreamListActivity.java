faf6e3fc0d4d8e2ac2ea01b8cd6a6664ef4a238b
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2011-01-18 15:37:17 +0000

    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@186 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/StreamListActivity.java
+++ src/net/sourceforge/servestream/StreamListActivity.java
@@ -46,7 +46,6 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
