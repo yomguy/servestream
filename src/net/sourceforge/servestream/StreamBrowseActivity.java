e088eb417bbd61cdc6ca33a533efb9c9f2798cf9
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2011-04-21 17:36:20 +0000

    Modified code to open a new activity when browsing streams and to use new URLUtils functionality
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@406 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/StreamBrowseActivity.java
+++ src/net/sourceforge/servestream/StreamBrowseActivity.java
@@ -33,6 +33,8 @@
 
 package net.sourceforge.servestream;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 
 import net.sourceforge.servestream.dbutils.Stream;
@@ -46,8 +48,10 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -68,18 +72,28 @@ import android.widget.AdapterView.OnItemClickListener;
 public class StreamBrowseActivity extends ListActivity {
 	public final static String TAG = "ServeStream.StreamBrowseActivity";
 	
-    StreamParser streamURLs = null;
+	private final static int START_ACTIVITY = 2;
+	private final static int ERROR_MESSAGE = 3;
+	
+	ArrayList<Stream> mStreamURLs = new ArrayList<Stream>();
 	Stream requestedStreamURL = null;
     
     ArrayAdapter<String> adapter = null;
-    ProgressDialog dialog = null;
     
 	protected StreamDatabase streamdb = null;
 	protected LayoutInflater inflater = null;
 	private Button mHomeButton = null;
 
-    private final Handler handler = new Handler();
-    private final UILoadingHelperClass uiLoadingThread = new UILoadingHelperClass();
+	protected Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			if (msg.what == START_ACTIVITY) {
+				beginActivity((Intent)msg.obj);
+			} else if (msg.what == ERROR_MESSAGE) {
+				cannotOpenURLMessage();
+			}
+		}
+	};
 	
     @Override
     public void onCreate(Bundle icicle) {
@@ -90,30 +104,27 @@ public class StreamBrowseActivity extends ListActivity {
 		this.setTitle(String.format("%s: %s",
 				getResources().getText(R.string.app_name),
 				getResources().getText(R.string.title_stream_browse)));        
-        
-		dialog = ProgressDialog.show(StreamBrowseActivity.this, "", 
-                "Loading. Please wait...", true);
 		
 		try {
 			Log.v(TAG, getIntent().getData().toString());			
-			requestedStreamURL = new Stream(getIntent().getData().toString());		
-	        streamURLs = new StreamParser(requestedStreamURL.getURL());
+			requestedStreamURL = new Stream(getIntent().getData().toString());
 		} catch (Exception ex) {
-			//TODO add handling here
 			ex.printStackTrace();
 		}
 	    	
-	    new DataLoadingThread(handler, uiLoadingThread, streamURLs);
+		HTMLParseAsyncTask htmlParseAsyncTask = new HTMLParseAsyncTask();
+		htmlParseAsyncTask.execute(requestedStreamURL);
 		
 		this.streamdb = new StreamDatabase(this);
 		
 		ListView list = this.getListView();
 		list.setFastScrollEnabled(true);
-		uiLoadingThread.setListView(this.getListView());
 
 		list.setOnItemClickListener(new OnItemClickListener() {
 			public synchronized void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		        handleStream(streamURLs.getParsedURL(position));
+				DetermineIntentAsyncTask determineIntentAsyncTask = new DetermineIntentAsyncTask();
+				determineIntentAsyncTask.setHandler(mHandler);
+				determineIntentAsyncTask.execute(mStreamURLs.get(position));
 			}
 		});
 		
@@ -232,37 +243,12 @@ public class StreamBrowseActivity extends ListActivity {
 		}
 	}
 	
-	public void handleStream(Stream stream) {
-		
-		Intent intent = null;
-		int contentTypeCode = -1;
-		
-		try {
-			contentTypeCode = URLUtils.getContentTypeCode(stream.getURL());
-			Log.v(TAG, "STREAM is: " + stream.getURL());
-		} catch (Exception ex) {
-			cannotOpenURLMessage();
-			return;
-		}
-		
-		if (contentTypeCode == URLUtils.DIRECTORY) {
-			dialog = ProgressDialog.show(StreamBrowseActivity.this, "", 
-	                "Loading. Please wait...", true);
-			try {
-				requestedStreamURL = stream;
-				streamURLs = new StreamParser(requestedStreamURL.getURL());
-		    	new DataLoadingThread(handler, uiLoadingThread, streamURLs);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		} else if (contentTypeCode == URLUtils.MEDIA_FILE) {
-			intent = new Intent(StreamBrowseActivity.this, StreamMediaActivity.class);
-			intent.setData(stream.getUri());
-			this.startActivity(intent);
-		} else if (contentTypeCode == URLUtils.NOT_FOUND) {
-			cannotOpenURLMessage();
-			return;
-		}
+	private void setStreamURLs(ArrayList<Stream> streamURLs) {
+		mStreamURLs = streamURLs;
+	}
+	
+	private void beginActivity(Intent intent) {
+		this.startActivity(intent);
 	}
 	
 	private void returnHome() {
@@ -340,51 +326,6 @@ public class StreamBrowseActivity extends ListActivity {
 			streamdb.saveStream(targetStream);
 		}
 	}
-
-	private class DataLoadingThread extends Thread {
-
-        private final Handler handler;
-        private final UILoadingHelperClass uiLoadingThread;
-        private final StreamParser streamURLs;
-
-        public DataLoadingThread(Handler handler, UILoadingHelperClass uiLoadingThread, StreamParser streamURLs) {
-            this.handler = handler;
-            this.uiLoadingThread = uiLoadingThread;
-            this.streamURLs = streamURLs;
-            this.start();
-        }
-
-        @Override
-        public void run() {
-        	
-    		streamURLs.getListing();
-            uiLoadingThread.setStreams(streamURLs.getParsedURLs());
-            handler.post(uiLoadingThread);
-   
-            // we are done retrieving, parsing and adding links
-            // to the list so dismiss the dialog
-            dialog.dismiss();
-        }
-    }
-	
-    private class UILoadingHelperClass implements Runnable {
-        
-    	private ListView m_listView = null;
-        private ArrayList<Stream> m_streams = null;
-
-        public void setStreams(ArrayList<Stream> streams){
-            this.m_streams = streams;
-        }
-
-        public void setListView(ListView listView){
-            m_listView = listView;
-        }
-
-        public void run() {
-    		StreamAdapter adapter = new StreamAdapter(StreamBrowseActivity.this, m_streams);
-        	m_listView.setAdapter(adapter);
-        }      
-    }
     
     private void cannotOpenURLMessage() {
 		new AlertDialog.Builder(StreamBrowseActivity.this)
@@ -394,4 +335,126 @@ public class StreamBrowseActivity extends ListActivity {
 			}
 			}).create().show();
     }
+    
+    public class HTMLParseAsyncTask extends AsyncTask<Stream, Void, StreamAdapter> {
+
+		ProgressDialog mDialog;
+		StreamAdapter adapter;
+		
+	    public HTMLParseAsyncTask() {
+	        super();
+	    }
+
+	    @Override
+	    protected void onPreExecute() {
+	    	mDialog = new ProgressDialog(StreamBrowseActivity.this);
+	        mDialog.setMessage("Loading. Please wait...");
+	        mDialog.setIndeterminate(true);
+	        mDialog.setCancelable(true);
+	        mDialog.show();
+	    }
+	    
+		@Override
+		protected StreamAdapter doInBackground(Stream... stream) {
+			StreamParser streamParser = null;
+			
+			try {
+				streamParser = new StreamParser(stream[0].getURL());
+				streamParser.getListing();
+			} catch (MalformedURLException ex) {
+				ex.printStackTrace();
+			}
+			
+			setStreamURLs(streamParser.getParsedURLs());
+	        return new StreamAdapter(StreamBrowseActivity.this, streamParser.getParsedURLs());
+		}
+
+		@Override
+		protected void onPostExecute(StreamAdapter adapter) {
+			setListAdapter(adapter);
+			mDialog.dismiss();
+		}
+		
+	}
+    
+    public class DetermineIntentAsyncTask extends AsyncTask<Stream, Void, Intent> {
+
+		ProgressDialog mDialog;
+		Handler mHandler;
+		
+	    public DetermineIntentAsyncTask() {
+	        super();
+	    }
+
+	    @Override
+	    protected void onPreExecute() {
+	    	mDialog = new ProgressDialog(StreamBrowseActivity.this);
+	        mDialog.setMessage(getString(R.string.opening_url_message));
+	        mDialog.setIndeterminate(true);
+	        mDialog.setCancelable(true);
+	        mDialog.show();
+	    }
+	    
+		@Override
+		protected Intent doInBackground(Stream... stream) {
+		    return handleStream(stream[0]);
+		}
+
+		@Override
+		protected void onPostExecute(Intent result) {
+			if (result != null) {
+				Message msg = Message.obtain();
+				msg.what = START_ACTIVITY;
+				msg.obj = result;
+				mHandler.sendMessage(msg);
+			} else {
+				Message msg = Message.obtain();
+				msg.what = ERROR_MESSAGE;
+				mHandler.sendMessage(msg);
+			}
+			
+			mDialog.dismiss();
+		}
+
+		public Intent handleStream(Stream stream) {
+			
+			Intent intent = null;
+			String contentTypeCode = null;
+			URLUtils urlUtils = null;
+			
+			try {
+				urlUtils = new URLUtils(stream.getURL());
+				Log.v(TAG, "STREAM is: " + stream.getURL());
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				//showURLNotFoundMessage();
+				return null;
+			}
+			
+			if (urlUtils.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			
+				contentTypeCode = urlUtils.getContentType();
+				
+				if (contentTypeCode.equalsIgnoreCase("text/html")) {
+					intent = new Intent(StreamBrowseActivity.this, StreamBrowseActivity.class);
+				} else { //if (contentTypeCode == URLUtils.MEDIA_FILE) {
+					intent = new Intent(StreamBrowseActivity.this, StreamMediaActivity.class);			
+				}
+		    }
+			
+			if (intent == null) {
+				//showURLNotFoundMessage();
+				return null;
+			} else {
+				intent.setData(stream.getUri());
+			}
+			
+			return intent;
+		}
+		
+		public void setHandler(Handler mHandler) {
+			this.mHandler = mHandler;
+		}
+			
+	}
 }
