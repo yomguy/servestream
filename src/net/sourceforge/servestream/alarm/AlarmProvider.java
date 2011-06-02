ec3102c775de8aef9296951c3d45e621a21d5263
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2011-06-02 06:09:03 +0000

    Set DATABASE_NAME to public
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@563 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/alarm/AlarmProvider.java
+++ src/net/sourceforge/servestream/alarm/AlarmProvider.java
@@ -61,7 +61,7 @@ public class AlarmProvider extends ContentProvider {
     }
 
     private static class DatabaseHelper extends SQLiteOpenHelper {
-        private static final String DATABASE_NAME = "alarms.db";
+        public static final String DATABASE_NAME = "alarms.db";
         private static final int DATABASE_VERSION = 5;
 
         public DatabaseHelper(Context context) {
