0e7f13aa8ce230ca5a6ac2430ec8100334f245d2
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2011-08-27 03:55:45 +0000

    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@708 b8d320ad-ce4d-463a-8333-51e6c77ad906

---
+++ src/net/sourceforge/servestream/activity/SettingsActivity.java
@@ -0,0 +1,67 @@
+/*
+ * ServeStream: A HTTP stream browser/player for Android
+ * Copyright 2010 William Seemann
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * ConnectBot: simple, powerful, open-source SSH client for Android
+ * Copyright 2007 Kenny Root, Jeffrey Sharkey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sourceforge.servestream.activity;
+
+import net.sourceforge.servestream.R;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class SettingsActivity extends PreferenceActivity {
+	private static final String TAG = SettingsActivity.class.getName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		try {
+			addPreferencesFromResource(R.xml.preferences);
+		} catch (ClassCastException e) {
+			Log.e(TAG, "Shared preferences are corrupt! Resetting to default values.");
+
+			SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+			// Blow away all the preferences
+			SharedPreferences.Editor editor = preferences.edit();
+			editor.clear();
+			editor.commit();
+
+			PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+
+			addPreferencesFromResource(R.xml.preferences);
+		}
+	}
+
+}
