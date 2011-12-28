ed5743a9b467f353787b84d5cc58843758c31eb8
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2011-12-28 05:45:57 +0000

    Changed activity call to use NowPlayingActivity
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@841 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/activity/MediaPlaybackActivity.java
+++ src/net/sourceforge/servestream/activity/MediaPlaybackActivity.java
@@ -515,12 +515,7 @@ public class MediaPlaybackActivity extends Activity implements SurfaceHolder.Cal
     		    	cycleRepeat();
     		        break;
         		case (R.id.menu_item_now_playing):
-        			//startActivity(new Intent(MediaPlaybackActivity.this, NowPlayingActivity.class));
-                    startActivity(
-                            new Intent(Intent.ACTION_EDIT)
-                            .setDataAndType(Uri.EMPTY, "vnd.android.cursor.dir/track")
-                            .putExtra("playlist", "nowplaying")
-                            );
+        			startActivity(new Intent(MediaPlaybackActivity.this, NowPlayingActivity.class));
         			break;
         		case (R.id.menu_item_sleep_timer): {
         			final String [] sleepTimerModes = getSleepTimerModes();
