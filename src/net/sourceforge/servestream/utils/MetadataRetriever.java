6bc35ebd2eab6b7da29b6158dbffc458efda548e
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2011-12-29 01:52:43 +0000

    Revised code to use the media provider when retrieving and storing metadata
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@851 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/utils/MetadataRetriever.java
+++ src/net/sourceforge/servestream/utils/MetadataRetriever.java
@@ -23,6 +23,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import net.sourceforge.servestream.provider.Media;
+import net.sourceforge.servestream.service.MediaPlaybackService;
+
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.XMPDM;
@@ -31,28 +34,108 @@ import org.apache.tika.parser.mp3.Mp3Parser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.SAXException;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
 public class MetadataRetriever {
 	
 	private static final String MP3_CONTENT_TYPE = "audio/mpeg";
 	
-	/**
-	 * 
-	 */
-	public MetadataRetriever() {
+	// This class cannot be instantiated
+	private MetadataRetriever() {
 		
 	}
 	
-	public static MediaFile retrieveMetadata(MediaFile mediaFile) {
+	/*
+	 * Retrieves metadata for an audio file and stores the information in the
+	 * corresponding media table row.
+	 */
+	public static void retrieve(Context context, long id) {
+		new RetrieveMetadataAsyncTask(context).execute(id);
+	}
+	
+	/*
+	 * Retrieves metadata for a set of audio files and stores the information in the
+	 * corresponding media table rows.
+	 */
+	public static void retrieve(Context context, long [] list) {
+		for (int i = 0; i < list.length; i++) {
+			new RetrieveMetadataAsyncTask(context).execute(list[i]);
+		}
+	}
+	
+	private static class RetrieveMetadataAsyncTask extends AsyncTask<Long, Void, Boolean> {
+	    
+		private Context mContext = null;
+		
+		public RetrieveMetadataAsyncTask(Context context) {
+	        super();
+	        mContext = context;
+	    }
+	    
+		@Override
+		protected Boolean doInBackground(Long ... list) {
+			String uri = getUri(mContext, list[0]);
+			
+			if (uri != null) {
+				Metadata metadata = retrieveMetadata(uri);
+				
+				if (metadata != null) {
+					updateMetadata(mContext, list[0], metadata);
+					
+					// send a broadcast so our activities can use the updated metadata 
+			        Intent intent = new Intent(MediaPlaybackService.META_CHANGED);
+					mContext.sendBroadcast(intent);
+				}
+			}
+			
+    		return true;
+		}
+    }
+	
+	private static String getUri(Context context, long id) {
+		String uri = null;
+		
+		// Form an array specifying which columns to return. 
+		String [] projection = new String [] { Media.MediaColumns.URI };
+
+		// Get the base URI for the Media Files table in the Media content provider.
+		Uri mediaFile =  Media.MediaColumns.CONTENT_URI;
+
+		// Make the query.
+		Cursor cursor = context.getContentResolver().query(mediaFile, 
+				projection,
+				Media.MediaColumns._ID + "= ? ",
+				new String [] { String.valueOf(id) },
+				null);    	
+	
+		if (cursor.moveToFirst()) {
+			int uriColumn = cursor.getColumnIndex(Media.MediaColumns.URI);
+			uri = cursor.getString(uriColumn);
+		}
+		
+		cursor.close();
+		
+		return uri;
+	}
+	
+	private static Metadata retrieveMetadata(String uri) {
+		Metadata metadata = null;		
 	    String contentType = null;
 	    HttpURLConnection conn = null;
 	    InputStream inputStream = null;	
 	    URL url;
 	    
-	    if (mediaFile == null)
-	    	return mediaFile;
+	    if (uri == null) {
+	    	return null;
+	    }
 	    
 		try {
-			url = new URL(mediaFile.getURL());
+			url = new URL(uri);
 	    		
 			conn = URLUtils.getConnection(url);
 	    	
@@ -63,16 +146,19 @@ public class MetadataRetriever {
 		    
 			contentType = conn.getContentType();
 	        
-			if (contentType == null)
-				return mediaFile;
+			if (contentType == null) {
+				return null;
+			}
 	    	
 			inputStream = conn.getInputStream();
 			
-			if (inputStream == null)
-				return mediaFile;
+			if (inputStream == null) {
+				return null;
+			}
 			
-			if (contentType.equalsIgnoreCase(MP3_CONTENT_TYPE))
-				mediaFile = retrieveMP3metadata(mediaFile, inputStream);
+			if (contentType.equalsIgnoreCase(MP3_CONTENT_TYPE)) {
+				metadata = retrieveMP3metadata(inputStream);
+			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -82,36 +168,41 @@ public class MetadataRetriever {
 		} catch (TikaException e) {
 			e.printStackTrace();
 		} finally {
-			closeInputStream(inputStream);
+			Utils.closeInputStream(inputStream);
 		}
-				
-		//Log.v(TAG, "retrieved metadata for" + mediaFile.getURL());
 		
-		return mediaFile;
+		return metadata;
 	}
 	
-	private static MediaFile retrieveMP3metadata(MediaFile mediaFile, InputStream inputStream) throws IOException, SAXException, TikaException {
+	private static Metadata retrieveMP3metadata(InputStream inputStream) throws IOException, SAXException, TikaException {
 		Mp3Parser mp3Parser = new Mp3Parser();
 		BodyContentHandler handler = new BodyContentHandler();
 	    Metadata metadata = new Metadata();
 		
 		mp3Parser.parse(inputStream, handler, metadata, new ParseContext());
-    
-		mediaFile.setTrack(metadata.get(Metadata.TITLE));
-		mediaFile.setArtist(metadata.get(XMPDM.ARTIST));
-		mediaFile.setAlbum(metadata.get(XMPDM.ALBUM));
 		
-		return mediaFile;
+		return metadata;
 	}
 	
-	private static void closeInputStream(InputStream inputStream) {
-		if (inputStream == null)
-			return;
+	private static int updateMetadata(Context context, long id, Metadata metadata) {
+		int rows = 0;
 		
-		try {
-			inputStream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// Form an array specifying which columns to return. 
+		ContentValues values = new ContentValues();
+		values.put(Media.MediaColumns.TITLE, metadata.get(Metadata.TITLE));
+		values.put(Media.MediaColumns.ALBUM, metadata.get(XMPDM.ALBUM));
+		values.put(Media.MediaColumns.ARTIST, metadata.get(XMPDM.ARTIST));
+
+		// Get the base URI for the Media Files table in the Media content provider.
+		Uri mediaFile =  Media.MediaColumns.CONTENT_URI;
+
+		// Execute the update.
+		rows = context.getContentResolver().update(mediaFile, 
+				values, 
+				Media.MediaColumns._ID + "= ? ", 
+				new String [] { String.valueOf(id) } );
+	
+		// return the number of rows updated.
+		return rows;
 	}
 }
