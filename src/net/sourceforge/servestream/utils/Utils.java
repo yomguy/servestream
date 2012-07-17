6d42805ff256f206a2aa87569c0deb0dceb91eac
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-07-17 04:03:44 +0000

    Added code to parse file extension
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@1060 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/utils/Utils.java
+++ src/net/sourceforge/servestream/utils/Utils.java
@@ -101,6 +101,84 @@ public class Utils {
     }
 	
     /**
+     * Gets the extension of a filename.
+     * <p>
+     * This method returns the textual part of the filename after the last dot.
+     * There must be no directory separator after the dot.
+     * <pre>
+     * foo.txt      --> "txt"
+     * a/b/c.jpg    --> "jpg"
+     * a/b.txt/c    --> ""
+     * a/b/c        --> ""
+     * </pre>
+     * <p>
+     * The output will be the same irrespective of the machine that the code is running on.
+     *
+     * @param filename the filename to retrieve the extension of.
+     * @return the extension of the file or an empty string if none exists or {@code null}
+     * if the filename is {@code null}.
+     */
+	public static String getExtension(String filename) {
+		if (filename == null) {
+			return null;
+		}
+		
+		int index = indexOfExtension(filename);
+		
+		if (index == -1) {
+			return "";
+		} else {
+			return filename.substring(index + 1);
+		}
+	}
+	
+	/**
+     * Returns the index of the last extension separator character, which is a dot.
+     * <p>
+     * This method also checks that there is no directory separator after the last dot.
+     * To do this it uses {@link #indexOfLastSeparator(String)} which will
+     * handle a file in either Unix or Windows format.
+     * <p>
+     * The output will be the same irrespective of the machine that the code is running on.
+     * 
+     * @param filename  the filename to find the last path separator in, null returns -1
+     * @return the index of the last separator character, or -1 if there
+     * is no such character
+     */
+	private static int indexOfExtension(String filename) {
+		if (filename == null) {
+			return -1;
+		}
+		
+		int extensionPos = filename.lastIndexOf('.');
+		int lastSeparator = indexOfLastSeparator(filename);
+		return lastSeparator > extensionPos ? -1 : extensionPos;
+	}
+	
+	/**
+     * Returns the index of the last extension separator character, which is a dot.
+     * <p>
+     * This method also checks that there is no directory separator after the last dot.
+     * To do this it uses {@link #indexOfLastSeparator(String)} which will
+     * handle a file in either Unix or Windows format.
+     * <p>
+     * The output will be the same irrespective of the machine that the code is running on.
+     * 
+     * @param filename  the filename to find the last path separator in, null returns -1
+     * @return the index of the last separator character, or -1 if there
+     * is no such character
+     */
+	private static int indexOfLastSeparator(String filename) {
+		if (filename == null) {
+			return -1;
+		}
+		
+		int lastUnixPos = filename.lastIndexOf('/');
+		int lastWindowsPos = filename.lastIndexOf('\\');
+		return Math.max(lastUnixPos, lastWindowsPos);
+	}
+    
+    /**
 	 * Closes a BufferedReader
 	 * 
 	 * @param reader A BufferedReader to close
