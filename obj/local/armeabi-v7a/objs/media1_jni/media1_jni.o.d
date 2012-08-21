543ae25647cc9391f64c23866338c7e35ade2741
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-08-21 02:28:08 +0000

    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@1112 b8d320ad-ce4d-463a-8333-51e6c77ad906

---
+++ obj/local/armeabi-v7a/objs/media1_jni/media1_jni.o.d
@@ -0,0 +1,197 @@
+obj/local/armeabi-v7a/objs/media1_jni/media1_jni.o: \
+ jni/metadata/media1_jni.c \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/android/log.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavformat/avformat.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/time.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcodec/avcodec.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/errno.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/errno.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/errno.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcore/samplefmt.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/avutil.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/common.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/ctype.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/inttypes.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/math.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/strings.h \
+ /home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/memory.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/attributes.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/avconfig.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/mem.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/avutil.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/error.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/mathematics.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/rational.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/intfloat_readwrite.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/log.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/pixfmt.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/cpu.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcore/audioconvert.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcore/avcore.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavformat/avio.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/common.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/log.h \
+ jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavdevice/avdevice.h
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavformat/avformat.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/time.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcodec/avcodec.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/errno.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/errno.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm/errno.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcore/samplefmt.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/avutil.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/common.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/ctype.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/inttypes.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/math.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/home/wseemann/Android/android-ndk-r7c/platforms/android-8/arch-arm/usr/include/memory.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/attributes.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/avconfig.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/mem.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/avutil.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/error.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/mathematics.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/rational.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/intfloat_readwrite.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/log.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/pixfmt.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/cpu.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcore/audioconvert.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavcore/avcore.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavformat/avio.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/common.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavutil/log.h:
+
+jni/ffmpeg/ffmpeg/armeabi-v7a/include/libavdevice/avdevice.h:
