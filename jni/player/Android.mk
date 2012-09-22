ff50264021d12ee8beba71dc6ab4b9ccce6c8d9e
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-09-22 05:31:18 +0000

    Initial check-in
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@1159 b8d320ad-ce4d-463a-8333-51e6c77ad906

---
+++ jni/player/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ffmpeg_player_jni
+LOCAL_CFLAGS := 
+LOCAL_SRC_FILES := ffmpeg_player_jni.c
+LOCAL_SHARED_LIBRARIES := libavformat libavcodec
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/lib -llog -lavformat -lavcodec
+
+include $(BUILD_SHARED_LIBRARY)
