bc7ba8fd8d64bd1ebf87cec8d43265e46e9ec943
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-10-27 23:48:27 +0000

    Cleaned up build script
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@1197 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- jni/metadata/Android.mk
+++ jni/metadata/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE := media1_jni
 LOCAL_CFLAGS := 
 LOCAL_SRC_FILES := media1_jni.c
 LOCAL_SHARED_LIBRARIES := libavformat libavutil
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/lib -llog -lavformat -lavutil
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
