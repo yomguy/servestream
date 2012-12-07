e2f3598fa32c1d8830297ef686c7810d2111779a
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-12-07 05:21:54 +0000

    Added libavcodec to LOCAL_SHARED_LIBRARIES
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@1235 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- jni/metadata/Android.mk
+++ jni/metadata/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := media1_jni
 LOCAL_CFLAGS := 
 LOCAL_SRC_FILES := media1_jni.c
-LOCAL_SHARED_LIBRARIES := libavformat libavutil
+LOCAL_SHARED_LIBRARIES := libavcodec libavformat libavutil
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
 LOCAL_LDLIBS := -llog
 
