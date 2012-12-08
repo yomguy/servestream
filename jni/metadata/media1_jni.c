157d40d77ea577f438242083312937d59335b200
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-12-08 06:15:59 +0000

    Refactored and optimized code
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@1237 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- jni/metadata/media1_jni.c
+++ jni/metadata/media1_jni.c
@@ -24,8 +24,7 @@
 const char *TAG = "Java_net_sourceforge_servestream_media_MediaMetadataRetriever";
 const char *DURATION = "duration";
 
-static AVDictionary *metadata = NULL;
-static int duration = 0;
+static AVFormatContext *pFormatCtx = NULL;
 static AVPacket album_art;
 static int found_album_art = 0;
 
@@ -45,16 +44,16 @@ Java_net_sourceforge_servestream_media_MediaMetadataRetriever_native_1init(JNIEn
     av_register_all();
 }
 
-int getDuration(AVFormatContext *pFormatCtx) {
-	if (!pFormatCtx) {
-		return 0;
+void getDuration(AVFormatContext *ic, char * value) {
+	int duration = 0;
+
+	if (ic) {
+		if (ic->duration != AV_NOPTS_VALUE) {
+			duration = ((ic->duration / AV_TIME_BASE) * 1000);
+		}
 	}
 
-	if (pFormatCtx->duration != AV_NOPTS_VALUE) {
-		return ((pFormatCtx->duration / AV_TIME_BASE) * 1000);
-	}
-
-	return 0;
+	sprintf(value, "%d", duration); // %i
 }
 
 AVPacket getAlbumArt(AVFormatContext *pFormatCtx) {
@@ -89,47 +88,45 @@ JNIEXPORT void JNICALL
 Java_net_sourceforge_servestream_media_MediaMetadataRetriever__1setDataSource(JNIEnv *env, jclass obj, jstring jpath) {
 	//__android_log_write(ANDROID_LOG_INFO, TAG, "setDataSource");
 
-	metadata = NULL;
-	duration = 0;
+	if (pFormatCtx) {
+		avformat_close_input(&pFormatCtx);
+	}
+
+	char duration[30] = "0";
 	//av_init_packet(&album_art);
 	found_album_art = 0;
 
-    AVFormatContext *fmt_ctx = NULL;
     const char *uri;
 
     uri = (*env)->GetStringUTFChars(env, jpath, NULL);
 
-    __android_log_write(ANDROID_LOG_INFO, TAG, uri);
+    //__android_log_write(ANDROID_LOG_INFO, TAG, uri);
 
-    if (avformat_open_input(&fmt_ctx, uri, NULL, NULL) != 0) {
+    if (avformat_open_input(&pFormatCtx, uri, NULL, NULL) != 0) {
 	    __android_log_write(ANDROID_LOG_INFO, TAG, "Metadata could not be retrieved");
-	    avformat_free_context(fmt_ctx);
-    	fmt_ctx = NULL;
+	    avformat_close_input(&pFormatCtx);
         (*env)->ReleaseStringUTFChars(env, jpath, uri);
     	return;
     }
 
-	if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
+	if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
 	    __android_log_write(ANDROID_LOG_INFO, TAG, "Metadata could not be retrieved");
-	    avformat_free_context(fmt_ctx);
-    	fmt_ctx = NULL;
+	    avformat_close_input(&pFormatCtx);
         (*env)->ReleaseStringUTFChars(env, jpath, uri);
     	return;
 	}
 
-    metadata = fmt_ctx->metadata;
-    duration = getDuration(fmt_ctx);
+	getDuration(pFormatCtx, duration);
+	av_dict_set(&pFormatCtx->metadata, DURATION, duration, 0);
     //album_art = getAlbumArt(fmt_ctx);
 
 	__android_log_write(ANDROID_LOG_INFO, TAG, "Found metadata");
 	/*AVDictionaryEntry *tag = NULL;
-	while ((tag = av_dict_get(metadata, "", tag, AV_DICT_IGNORE_SUFFIX))) {
+	while ((tag = av_dict_get(pFormatCtx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX))) {
     	__android_log_write(ANDROID_LOG_INFO, TAG, tag->key);
     	__android_log_write(ANDROID_LOG_INFO, TAG, tag->value);
     }*/
 
-	av_free(fmt_ctx);
-    fmt_ctx = NULL;
     (*env)->ReleaseStringUTFChars(env, jpath, uri);
 }
 
@@ -137,27 +134,15 @@ JNIEXPORT jstring JNICALL
 Java_net_sourceforge_servestream_media_MediaMetadataRetriever_extractMetadata(JNIEnv *env, jclass obj, jstring jkey) {
 	//__android_log_write(ANDROID_LOG_INFO, TAG, "extractMetadata");
 
-	AVDictionaryEntry *tag = NULL;
     const char *key;
     jstring value = NULL;
 
     key = (*env)->GetStringUTFChars(env, jkey, NULL) ;
 
-	if (!key) {
-        (*env)->ReleaseStringUTFChars(env, jkey, key);
-		return value;
-	}
-
-	if (strcmp(key, DURATION) == 0) {
-		char string_duration[30];
-		sprintf(string_duration, "%d", duration); // %i
-    	value = (*env)->NewStringUTF(env, string_duration);
-	} else {
-	    tag = av_dict_get(metadata, key, NULL, AV_DICT_IGNORE_SUFFIX);
-
-	    if (tag) {
-	    	value = (*env)->NewStringUTF(env, tag->value);
-	    }
+	if (key) {
+		if (av_dict_get(pFormatCtx->metadata, key, NULL, AV_DICT_IGNORE_SUFFIX)) {
+			value = (*env)->NewStringUTF(env, av_dict_get(pFormatCtx->metadata, key, NULL, AV_DICT_IGNORE_SUFFIX)->value);
+		}
 	}
 
     (*env)->ReleaseStringUTFChars(env, jkey, key);
@@ -220,14 +205,13 @@ Java_net_sourceforge_servestream_media_MediaMetadataRetriever__1getEmbeddedPictu
 
 JNIEXPORT void JNICALL
 Java_net_sourceforge_servestream_media_MediaMetadataRetriever_release(JNIEnv *env, jclass obj) {
-	__android_log_write(ANDROID_LOG_INFO, TAG, "release");
+	//__android_log_write(ANDROID_LOG_INFO, TAG, "release");
 
     //av_free_packet(&album_art);
 
-    if (metadata) {
-        metadata = NULL;
-        //av_free(metadata);
-        //av_dict_free(&metadata);
+    if (pFormatCtx) {
+        avformat_close_input(&pFormatCtx);
     }
 }
+
 
