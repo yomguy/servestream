dd671f15bd2a25ea5e52502c5a96d41a97091ea8
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-12-13 08:13:12 +0000

    Cleaned up code, removed unused methods
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@1239 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/media/MediaMetadataRetriever.java
+++ src/net/sourceforge/servestream/media/MediaMetadataRetriever.java
@@ -17,16 +17,6 @@
 
 package net.sourceforge.servestream.media;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-
 /**
  * MediaMetadataRetriever class provides a unified interface for retrieving
  * frame and meta data from an input media file.
@@ -38,13 +28,8 @@ public class MediaMetadataRetriever
         native_init();
     }
 
-    // The field below is accessed by native methods
-    private int mNativeContext;
- 
-    private static final int EMBEDDED_PICTURE_TYPE_ANY = 0xFFFF;
-
     public MediaMetadataRetriever() {
-        //native_setup();
+    
     }
 
     /**
@@ -55,15 +40,7 @@ public class MediaMetadataRetriever
      * @param path The path of the input media file.
      * @throws IllegalArgumentException If the path is invalid.
      */
-    public void setDataSource(String path) throws IllegalArgumentException {
-    	if (path == null) {
-    		throw new IllegalArgumentException();
-    	}
-    	
-    	_setDataSource(path);
-    }
-
-    public native int _setDataSource(String path);
+    public native void setDataSource(String path) throws IllegalArgumentException;
     
     /**
      * Call this method after setDataSource(). This method retrieves the 
@@ -79,122 +56,13 @@ public class MediaMetadataRetriever
     public native String extractMetadata(String key);
 
     /**
-     * Call this method after setDataSource(). This method finds a
-     * representative frame close to the given time position by considering
-     * the given option if possible, and returns it as a bitmap. This is
-     * useful for generating a thumbnail for an input data source or just
-     * obtain and display a frame at the given time position.
-     *
-     * @param timeUs The time position where the frame will be retrieved.
-     * When retrieving the frame at the given time position, there is no
-     * guarantee that the data source has a frame located at the position.
-     * When this happens, a frame nearby will be returned. If timeUs is
-     * negative, time position and option will ignored, and any frame
-     * that the implementation considers as representative may be returned.
-     *
-     * @param option a hint on how the frame is found. Use
-     * {@link #OPTION_PREVIOUS_SYNC} if one wants to retrieve a sync frame
-     * that has a timestamp earlier than or the same as timeUs. Use
-     * {@link #OPTION_NEXT_SYNC} if one wants to retrieve a sync frame
-     * that has a timestamp later than or the same as timeUs. Use
-     * {@link #OPTION_CLOSEST_SYNC} if one wants to retrieve a sync frame
-     * that has a timestamp closest to or the same as timeUs. Use
-     * {@link #OPTION_CLOSEST} if one wants to retrieve a frame that may
-     * or may not be a sync frame but is closest to or the same as timeUs.
-     * {@link #OPTION_CLOSEST} often has larger performance overhead compared
-     * to the other options if there is no sync frame located at timeUs.
-     *
-     * @return A Bitmap containing a representative video frame, which 
-     *         can be null, if such a frame cannot be retrieved.
-     */
-    public Bitmap getFrameAtTime(long timeUs, int option) {
-        if (option < OPTION_PREVIOUS_SYNC ||
-            option > OPTION_CLOSEST) {
-            throw new IllegalArgumentException("Unsupported option: " + option);
-        }
-
-        return _getFrameAtTime(timeUs, option);
-    }
-
-    /**
-     * Call this method after setDataSource(). This method finds a
-     * representative frame close to the given time position if possible,
-     * and returns it as a bitmap. This is useful for generating a thumbnail
-     * for an input data source. Call this method if one does not care
-     * how the frame is found as long as it is close to the given time;
-     * otherwise, please call {@link #getFrameAtTime(long, int)}.
-     *
-     * @param timeUs The time position where the frame will be retrieved.
-     * When retrieving the frame at the given time position, there is no
-     * guarentee that the data source has a frame located at the position.
-     * When this happens, a frame nearby will be returned. If timeUs is
-     * negative, time position and option will ignored, and any frame
-     * that the implementation considers as representative may be returned.
-     *
-     * @return A Bitmap containing a representative video frame, which
-     *         can be null, if such a frame cannot be retrieved.
-     *
-     * @see #getFrameAtTime(long, int)
-     */
-    public Bitmap getFrameAtTime(long timeUs) {
-        return getFrameAtTime(timeUs, OPTION_CLOSEST_SYNC);
-    }
-
-    /**
-     * Call this method after setDataSource(). This method finds a
-     * representative frame at any time position if possible,
-     * and returns it as a bitmap. This is useful for generating a thumbnail
-     * for an input data source. Call this method if one does not
-     * care about where the frame is located; otherwise, please call
-     * {@link #getFrameAtTime(long)} or {@link #getFrameAtTime(long, int)}
-     *
-     * @return A Bitmap containing a representative video frame, which
-     *         can be null, if such a frame cannot be retrieved.
-     *
-     * @see #getFrameAtTime(long)
-     * @see #getFrameAtTime(long, int)
-     */
-    public Bitmap getFrameAtTime() {
-        return getFrameAtTime(-1, OPTION_CLOSEST_SYNC);
-    }
-
-    private native Bitmap _getFrameAtTime(long timeUs, int option);
-
-    
-    /**
      * Call this method after setDataSource(). This method finds the optional
      * graphic or album/cover art associated associated with the data source. If
      * there are more than one pictures, (any) one of them is returned.
      * 
-     * @param context the Context to use
      * @return null if no such graphic is found.
      */
-    public byte[] getEmbeddedPicture(Context context) {
-    	byte [] blob = null;
-    	
-        String path = getEmbeddedPicture(context.getExternalCacheDir() + "/picture.tmp");
-        
-        if (path != null) {
-        	InputStream is;
-        	
-			try {
-				is = new BufferedInputStream(new FileInputStream(path));
-				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        	
-				while (is.available() > 0) {
-					bos.write(is.read());
-				}
-
-				blob = bos.toByteArray();
-			} catch (FileNotFoundException e) {
-			} catch (IOException e) {
-			}
-        }
-        
-        return blob;
-    }
-
-    private native String getEmbeddedPicture(String path);
+    public native String getEmbeddedPicture(String path);
 
     /**
      * Call this method after setDataSource(). This method finds the optional
@@ -214,58 +82,18 @@ public class MediaMetadataRetriever
      * allocated internally.
      */
     public native void release();
-    //private native void native_setup();
     private static native void native_init();
 
     //private native final void native_finalize();
 
-    /*@Override
+    @Override
     protected void finalize() throws Throwable {
         try {
-            native_finalize();
+            //native_finalize();
         } finally {
             super.finalize();
         }
-    }*/
-
-    /**
-     * Option used in method {@link #getFrameAtTime(long, int)} to get a
-     * frame at a specified location.
-     *
-     * @see #getFrameAtTime(long, int)
-     */
-    /**
-     * This option is used with {@link #getFrameAtTime(long, int)} to retrieve
-     * a sync (or key) frame associated with a data source that is located
-     * right before or at the given time.
-     *
-     * @see #getFrameAtTime(long, int)
-     */
-    public static final int OPTION_PREVIOUS_SYNC    = 0x00;
-    /**
-     * This option is used with {@link #getFrameAtTime(long, int)} to retrieve
-     * a sync (or key) frame associated with a data source that is located
-     * right after or at the given time.
-     *
-     * @see #getFrameAtTime(long, int)
-     */
-    public static final int OPTION_NEXT_SYNC        = 0x01;
-    /**
-     * This option is used with {@link #getFrameAtTime(long, int)} to retrieve
-     * a sync (or key) frame associated with a data source that is located
-     * closest to (in time) or at the given time.
-     *
-     * @see #getFrameAtTime(long, int)
-     */
-    public static final int OPTION_CLOSEST_SYNC     = 0x02;
-    /**
-     * This option is used with {@link #getFrameAtTime(long, int)} to retrieve
-     * a frame (not necessarily a key frame) associated with a data source that
-     * is located closest to or at the given time.
-     *
-     * @see #getFrameAtTime(long, int)
-     */
-    public static final int OPTION_CLOSEST          = 0x03;
+    }
 
     /**
      * The metadata key to retrieve the name of the set this work belongs to.
