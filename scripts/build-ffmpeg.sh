e6fc87d57a0993269c243ec499f3a214b8699182
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2012-12-29 00:13:12 +0000

    Modified build script to use absolute paths instead of relative ones
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@1298 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 WORKING_DIR=`pwd`
+SCRIPTS_DIR=$WORKING_DIR/scripts
+FFMPEG_BUILD_DIR=$WORKING_DIR/ffmpeg
+TARGET_ARMEABI_DIR=$WORKING_DIR/jni/ffmpeg/ffmpeg/armeabi
+TARGET_ARMEABIV7A_DIR=$WORKING_DIR/jni/ffmpeg/ffmpeg/armeabi-v7a
+TARGET_X86_DIR=$WORKING_DIR/jni/ffmpeg/ffmpeg/x86
+TARGET_MIPS_DIR=$WORKING_DIR/jni/ffmpeg/ffmpeg/mips
+SO_DIR=$FFMPEG_BUILD_DIR/build/ffmpeg
+
 export NDK=`grep ndk.dir local.properties | cut -d'=' -f2`
 
 if [ "$NDK" = "" ] || [ ! -d $NDK ]; then
@@ -15,37 +23,50 @@ if [ ! -d ffmpeg ]; then
     # Unpackage the FFmpeg archive
     tar -xvf ffmpeg-0.11.1-android-2012-09-18.tar.gz
     rm ffmpeg-0.11.1-android-2012-09-18.tar.gz
-    mv ffmpeg-0.11.1-android-2012-09-18 ffmpeg
+    mv ffmpeg-0.11.1-android-2012-09-18 $FFMPEG_BUILD_DIR
 
     # Prepare the FFmpeg archive for building
-    cd ffmpeg
+    cd $FFMPEG_BUILD_DIR
     ./extract.sh
 
+    cd $WORKING_DIR
+
     # Move the build scripts to the FFmpeg build folder
-    cp ../scripts/*.sh .
+    cp $SCRIPTS_DIR/*.sh $FFMPEG_BUILD_DIR  
 
     # Make the build scripts executable
-    chmod +x arm-build.sh
-    chmod +x x86-build.sh
-    chmod +x mips-build.sh
+    chmod +x $FFMPEG_BUILD_DIR/arm-build.sh
+    chmod +x $FFMPEG_BUILD_DIR/x86-build.sh
+    chmod +x $FFMPEG_BUILD_DIR/mips-build.sh
 fi
 
-if [ ! -d ../jni/ffmpeg/ffmpeg/arm* ]; then
+if [ ! -d $TARGET_ARMEABI_DIR ] || [ ! -d $TARGET_ARMEABIV7A_DIR ]; then
     # Build FFmpeg from ARM architecture and copy to the JNI folder
+    cd $FFMPEG_BUILD_DIR
     ./arm-build.sh
-    cp -r build/ffmpeg/* ../jni/ffmpeg/ffmpeg
+    cd $WORKING_DIR
+    echo "Copying FFmpeg .so files from $SO_DIR to $TARGET_ARMEABI_DIR"
+    cp -r $SO_DIR/armeabi $TARGET_ARMEABI_DIR
+    echo "Copying FFmpeg .so files from $SO_DIR to $TARGET_ARMEABIV7A_DIR"
+    cp -r $SO_DIR/armeabi-v7a $TARGET_ARMEABIV7A_DIR
 fi
 
 if [ ! -d ../jni/ffmpeg/ffmpeg/x86 ]; then
     # Build FFmpeg from x86 architecture and copy to the JNI folder
+    cd $FFMPEG_BUILD_DIR
     ./x86-build.sh
-    cp -r build/ffmpeg/* ../jni/ffmpeg/ffmpeg
+    cd $WORKING_DIR
+    echo "Copying FFmpeg .so files from $SO_DIR to $TARGET_MIPS_DIR"
+    cp -r $SO_DIR/x86 $TARGET_X86_DIR
 fi
 
 if [ ! -d ../jni/ffmpeg/ffmpeg/mips ]; then
     # Build FFmpeg from MIPS architecture and copy to the JNI folder
+    cd $FFMPEG_BUILD_DIR
     ./mips-build.sh
-    cp -r build/ffmpeg/* ../jni/ffmpeg/ffmpeg
+    cd $WORKING_DIR
+    echo "Copying FFmpeg .so files from $SO_DIR to $TARGET_MIPS_DIR"
+    cp -r $SO_DIR/mips $TARGET_MIPS_DIR
 fi
 
 echo Native build complete, exiting...
