f4902597577544ca42e05681b463835b2c3c88e1
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2013-03-21 21:29:56 +0000

    Refactored code to support multiple concurrent instances
    git-svn-id: http://svn.code.sf.net/p/servestream/code/trunk@1399 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- jni/metadata/Android.mk
+++ jni/metadata/Android.mk
@@ -4,7 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := media1_jni
 LOCAL_CFLAGS := 
-LOCAL_SRC_FILES := media1_jni.c
+LOCAL_SRC_FILES := MediaMetadataRetriever.cpp \
+        ffmpeg_mediametadataretriever.c  \
+	mediametadataretriever.cpp 
 LOCAL_SHARED_LIBRARIES := libavcodec libavformat libavutil
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
 LOCAL_LDLIBS := -llog
