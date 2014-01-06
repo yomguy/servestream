3e3dea913f1265d672c04ffedeb8542b811a2b52
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2014-01-06 00:55:07 +0000

    Accept all server certificates
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@1607 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/transport/HTTPS.java
+++ src/net/sourceforge/servestream/transport/HTTPS.java
@@ -21,13 +21,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.Authenticator;
+
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Map;
 
 import net.sourceforge.servestream.bean.UriBean;
@@ -177,7 +185,9 @@ public class HTTPS extends AbsTransport {
     		url = uri.getURL();
     	}
 
-    	conn = (HttpsURLConnection) url.openConnection();        	
+    	trustAllHosts();
+    	conn = (HttpsURLConnection) url.openConnection();   
+    	conn.setHostnameVerifier(DO_NOT_VERIFY);
     	conn.setConnectTimeout(6000);
     	conn.setReadTimeout(6000);
 	    conn.setRequestMethod("GET");
@@ -299,5 +309,42 @@ public class HTTPS extends AbsTransport {
 	@Override
 	public boolean isPotentialPlaylist() {
 		return true;
+	}
+	
+	// always verify the host - dont check for certificate
+	private final HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+	        public boolean verify(String hostname, SSLSession session) {
+	                return true;
+	        }
+	};
+
+	/**
+	 * Trust every server - dont check for any certificate
+	 */
+	private void trustAllHosts() {
+		// Create a trust manager that does not validate certificate chains
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return new java.security.cert.X509Certificate[] {};
+			}
+
+			public void checkClientTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+			}
+
+			public void checkServerTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		try {
+			SSLContext sc = SSLContext.getInstance("TLS");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection
+			.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
