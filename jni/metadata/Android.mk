0e21208e72d8c4a9c8bd0eccb7746e2330f34d16
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2014-01-12 06:02:15 +0000

    Update code to use FFmpegMediaMetadataRetriever
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@1627 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- jni/metadata/Android.mk
+++ jni/metadata/Android.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ffmpeg_mediametadataretriever_jni
 LOCAL_CFLAGS := 
 LOCAL_SRC_FILES := net_sourceforge_servestream_media_MediaMetadataRetriever.cpp \
-        ffmpeg_mediametadataretriever.c  \
-	mediametadataretriever.cpp 
+	mediametadataretriever.cpp \
+        ffmpeg_mediametadataretriever.c
 LOCAL_SHARED_LIBRARIES := libswscale libavcodec libavformat libavutil
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
 LOCAL_LDLIBS := -llog
