80bd67a102e1e482ab65052b3778b50836e688a1
wseemann <wseemann@b8d320ad-ce4d-463a-8333-51e6c77ad906>
2014-08-05 21:22:49 +0000

    Fix editor issue
    git-svn-id: svn://svn.code.sf.net/p/servestream/code/trunk@1755 b8d320ad-ce4d-463a-8333-51e6c77ad906

--- src/net/sourceforge/servestream/fragment/UriEditorFragment.java
+++ src/net/sourceforge/servestream/fragment/UriEditorFragment.java
@@ -1,278 +1,158 @@
-/*
- * ServeStream: A HTTP stream browser/player for Android
- * Copyright 2014 William Seemann
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.sourceforge.servestream.fragment;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import net.sourceforge.servestream.R;
+import net.sourceforge.servestream.bean.UriBean;
 import net.sourceforge.servestream.database.StreamDatabase;
-import net.sourceforge.servestream.preference.PreferenceFragment;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
-import android.preference.ListPreference;
+import android.os.Handler;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 
-public class UriEditorFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
-	public class CursorPreferenceHack implements SharedPreferences {
-		protected final String table;
-		protected final long id;
+public class UriEditorFragment extends net.sourceforge.servestream.preference.PreferenceFragment
+    	implements Preference.OnPreferenceChangeListener {
+	
+	private HashMap<String, String> mValues = new HashMap<String, String>();
+	
+	private EditTextPreference mNickname;
+	private EditTextPreference mProtocol;
+	private EditTextPreference mUsername;
+	private EditTextPreference mPassword;
+	private EditTextPreference mHostname;
+	private EditTextPreference mPort;
+	private EditTextPreference mPath;
+	private EditTextPreference mQuery;
+	private EditTextPreference mReference;
 
-		protected Map<String, String> values = new HashMap<String, String>();
-
-		public CursorPreferenceHack(String table, long id) {
-			this.table = table;
-			this.id = id;
-
-			cacheValues();
-		}
-
-		protected final void cacheValues() {
-			// fill a cursor and cache the values locally
-			// this makes sure we dont have any floating cursor to dispose later
-
-			SQLiteDatabase db = m_streamdb.getReadableDatabase();
-			Cursor cursor = db.query(table, null, "_id = ?",
-					new String[] { String.valueOf(id) }, null, null, null);
-
-			if (cursor.moveToFirst()) {
-				for(int i = 0; i < cursor.getColumnCount(); i++) {
-					String key = cursor.getColumnName(i);
-					String value = cursor.getString(i);
-					values.put(key, value);
-				}
-			}
-			cursor.close();
-			db.close();
-		}
-
-		public boolean contains(String key) {
-			return values.containsKey(key);
-		}
-
-		public class Editor implements SharedPreferences.Editor {
-
-			private ContentValues update = new ContentValues();
-
-			public SharedPreferences.Editor clear() {
-				update = new ContentValues();
-				return this;
-			}
-
-			public boolean commit() {
-				//Log.d(this.getClass().toString(), "commit() changes back to database");
-				SQLiteDatabase db = m_streamdb.getWritableDatabase();
-				db.update(table, update, "_id = ?", new String[] { String.valueOf(id) });
-				db.close();
-
-				// make sure we refresh the parent cached values
-				cacheValues();
-
-				// and update any listeners
-				for(OnSharedPreferenceChangeListener listener : listeners) {
-					listener.onSharedPreferenceChanged(CursorPreferenceHack.this, null);
-				}
-
-				return true;
-			}
-
-			public android.content.SharedPreferences.Editor putBoolean(String key, boolean value) {
-				return this.putString(key, Boolean.toString(value));
-			}
-
-			public android.content.SharedPreferences.Editor putFloat(String key, float value) {
-				return this.putString(key, Float.toString(value));
-			}
-
-			public android.content.SharedPreferences.Editor putInt(String key, int value) {
-				return this.putString(key, Integer.toString(value));
-			}
-
-			public android.content.SharedPreferences.Editor putLong(String key, long value) {
-				return this.putString(key, Long.toString(value));
-			}
-
-			public android.content.SharedPreferences.Editor putString(String key, String value) {
-				//Log.d(this.getClass().toString(), String.format("Editor.putString(key=%s, value=%s)", key, value));
-				
-	            // ensure the port is not null
-				if (key.equals("port") && value != null && value.equals("")) {
-					value = "-1";
-				}
-				
-				if (value != null && value.equals("")) {
-					value = null;
-					update.put(key, value);
-				} else {
-					update.put(key, value);
-				}
-				
-				return this;
-			}
-
-			public android.content.SharedPreferences.Editor remove(String key) {
-				//Log.d(this.getClass().toString(), String.format("Editor.remove(key=%s)", key));
-				update.remove(key);
-				return this;
-			}
-
-			// Gingerbread compatibility
-			public void apply() {
-				commit();
-			}
-
-			public android.content.SharedPreferences.Editor putStringSet(
-					String arg0, Set<String> arg1) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-		}
-
-
-		public Editor edit() {
-			//Log.d(this.getClass().toString(), "edit()");
-			return new Editor();
-		}
-
-		public Map<String, ?> getAll() {
-			return values;
-		}
-
-		public boolean getBoolean(String key, boolean defValue) {
-			return Boolean.valueOf(this.getString(key, Boolean.toString(defValue)));
-		}
-
-		public float getFloat(String key, float defValue) {
-			return Float.valueOf(this.getString(key, Float.toString(defValue)));
-		}
-
-		public int getInt(String key, int defValue) {
-			return Integer.valueOf(this.getString(key, Integer.toString(defValue)));
-		}
-
-		public long getLong(String key, long defValue) {
-			return Long.valueOf(this.getString(key, Long.toString(defValue)));
-		}
-
-		public String getString(String key, String defValue) {
-			//Log.d(this.getClass().toString(), String.format("getString(key=%s, defValue=%s)", key, defValue));
-
-			if(!values.containsKey(key)) return defValue;
-			return values.get(key);
-		}
-
-		protected List<OnSharedPreferenceChangeListener> listeners = new LinkedList<OnSharedPreferenceChangeListener>();
-
-		public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
-			listeners.add(listener);
-		}
-
-		public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
-			listeners.remove(listener);
-		}
-
-		public Set<String> getStringSet(String arg0, Set<String> arg1) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-	}
-
-	protected static final String TAG = UriEditorFragment.class.getName();
-
-	protected StreamDatabase m_streamdb = null;
-
-	private CursorPreferenceHack pref;
-
+	private UriBean mBean;
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		
+        addPreferencesFromResource(R.xml.stream_prefs);
+        
+        long streamId = getActivity().getIntent().getLongExtra(Intent.EXTRA_TITLE, -1);
 
-		if (savedInstanceState == null) {
-			long streamId = getActivity().getIntent().getLongExtra(Intent.EXTRA_TITLE, -1);
+        mNickname = (EditTextPreference) findPreference("nickname");
+        mNickname.setOnPreferenceChangeListener(mListener);
+        mValues.put("nickname", StreamDatabase.FIELD_STREAM_NICKNAME);
+        
+        mProtocol = (EditTextPreference) findPreference("protocol");
+        mProtocol.setOnPreferenceChangeListener(mListener);
+        mValues.put("protocol", StreamDatabase.FIELD_STREAM_PROTOCOL);
+        
+        mUsername = (EditTextPreference) findPreference("username");
+        mUsername.setOnPreferenceChangeListener(mListener);
+        mValues.put("username", StreamDatabase.FIELD_STREAM_USERNAME);
+        
+        mPassword = (EditTextPreference) findPreference("password");
+        mPassword.setOnPreferenceChangeListener(mListener);
+        mValues.put("password", StreamDatabase.FIELD_STREAM_PASSWORD);
+        
+        mHostname = (EditTextPreference) findPreference("hostname");
+        mHostname.setOnPreferenceChangeListener(mListener);
+        mValues.put("hostname", StreamDatabase.FIELD_STREAM_HOSTNAME);
 
-			m_streamdb = new StreamDatabase(getActivity());
-
-			pref = new CursorPreferenceHack(StreamDatabase.TABLE_STREAMS, streamId);
-			pref.registerOnSharedPreferenceChangeListener(this);
-
-			addPreferencesFromResource(R.xml.stream_prefs);
-
-			updateSummaries();
-		}
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-
-		if(this.m_streamdb == null)
-			this.m_streamdb = new StreamDatabase(getActivity());
-	}
-
-	@Override
-	public void onStop() {
-		super.onStop();
-
-		if (this.m_streamdb != null) {
-			this.m_streamdb.close();
-			this.m_streamdb = null;
-		}
+        mPort = (EditTextPreference) findPreference("port");
+        mPort.setOnPreferenceChangeListener(mListener);
+        mValues.put("port", StreamDatabase.FIELD_STREAM_PORT);
+        
+        mPath = (EditTextPreference) findPreference("path");
+        mPath.setOnPreferenceChangeListener(mListener);
+        mValues.put("path", StreamDatabase.FIELD_STREAM_PATH);
+        
+        mQuery = (EditTextPreference) findPreference("query");
+        mQuery.setOnPreferenceChangeListener(mListener);
+        mValues.put("query", StreamDatabase.FIELD_STREAM_QUERY);
+        
+        mReference = (EditTextPreference) findPreference("reference");
+        mReference.setOnPreferenceChangeListener(mListener);
+        mValues.put("reference", StreamDatabase.FIELD_STREAM_REFERENCE);
+        
+        StreamDatabase database = new StreamDatabase(getActivity());
+        mBean = database.findUri((int) streamId);
+        database.close();
+        
+        updatePrefs(mBean);
 	}
 	
-	private void updateSummaries() {
-		// for all text preferences, set hint as current database value
-		for (String key : this.pref.values.keySet()) {
-			Preference pref = this.findPreference(key);
-			if(pref == null) continue;
-			if(pref instanceof CheckBoxPreference) continue;
-			CharSequence value = this.pref.getString(key, "");
+	private OnPreferenceChangeListener mListener = new OnPreferenceChangeListener() {
 
-            // mask the password preference
-			if (key.equals("password") && value != null) {
-				value = new String(new char[value.length()]).replace("\0", "*");
-			}
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+	    	if (preference.getKey().equals("port") && newValue != null && newValue.equals("")) {
+	    		newValue = "-1";
+	    	}
 			
-			if (pref instanceof ListPreference) {
-				ListPreference listPref = (ListPreference) pref;
-				int entryIndex = listPref.findIndexOfValue((String) value);
-				if (entryIndex >= 0)
-					value = listPref.getEntries()[entryIndex];
-			}
-
-			pref.setSummary(value);
+	    	if (newValue != null && newValue.equals("")) {
+	    		newValue = null;
+	    	}
+	    	
+	        String val = (String) newValue;
+            // Set the summary based on the new label.
+			preference.setSummary(val);
+            if (val != null && !val.equals(((EditTextPreference) preference).getText())) {
+            	// Call through to the generic listener.
+                return UriEditorFragment.this.onPreferenceChange(preference,
+                    newValue);
+            }
+            return true;
 		}
+		
+	};
+	
+    // Used to post runnables asynchronously.
+    private static final Handler sHandler = new Handler();
+	
+    @Override
+    public boolean onPreferenceChange(final Preference preference, final Object newValue) {
+        // Asynchronously save the alarm since this method is called _before_
+        // the value of the preference has changed.
+        sHandler.post(new Runnable() {
+            public void run() {
+            	saveUri(preference, newValue);
+            }
+        });
+        return true;
+    }
+	
+    private void updatePrefs(UriBean bean) {
+    	mNickname.setSummary(bean.getNickname());
+    	mNickname.setText(bean.getNickname());
+    	mProtocol.setSummary(bean.getProtocol());
+    	mProtocol.setText(bean.getProtocol());
+    	mUsername.setSummary(bean.getUsername());
+    	mUsername.setText(bean.getUsername());
+    	mPassword.setSummary(bean.getPassword());
+    	mPassword.setText(bean.getPassword());
+    	mHostname.setSummary(bean.getHostname());
+    	mHostname.setText(bean.getHostname());
+    	mPort.setSummary(String.valueOf(bean.getPort()));
+    	mPort.setText(String.valueOf(bean.getPort()));
+    	mPath.setSummary(bean.getPath());
+    	mPath.setText(bean.getPath());
+    	mQuery.setSummary(bean.getQuery());
+    	mQuery.setText(bean.getQuery());
+    	mReference.setSummary(bean.getReference());
+    	mReference.setText(bean.getReference());
+    }
 
-	}
-
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		// update values on changed preference
-		this.updateSummaries();
-
-	}
+    private void saveUri(final Preference preference, Object newValue) {
+    	String value = (String) newValue;
+    	
+    	ContentValues values = new ContentValues();
+    	values.put(mValues.get(preference.getKey()), value);
+    	
+    	StreamDatabase database = new StreamDatabase(getActivity());
+    	SQLiteDatabase db = database.getWritableDatabase();
+    	db.update(StreamDatabase.TABLE_STREAMS, values, "_id = ?", new String[] { String.valueOf(mBean.getId()) });
+    	database.close();
+    	db.close();
+    }
 }
